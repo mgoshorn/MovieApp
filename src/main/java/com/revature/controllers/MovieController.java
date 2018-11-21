41bc846a3e75fe3b89fa996d69e9345e37e06fd2
Mitch Goshorn <msgoshorn@gmail.com>
2018-11-21 11:28:49 -0500

    Added cross origin config for controllers

--- src/main/java/com/revature/controllers/MovieController.java
+++ src/main/java/com/revature/controllers/MovieController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,7 @@ import com.revature.services.MovieService;
 
 @RestController
 @RequestMapping("movies")
+@CrossOrigin(methods={RequestMethod.GET, RequestMethod.POST}, origins="*")
 public class MovieController {
 
 	private MovieService movieService;
